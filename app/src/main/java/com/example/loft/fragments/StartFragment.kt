854d73ae02fd79be78b05026af9731a3efae8fdd
Bharath V Poojary <bharathbillawa@gmail.com>
2020-06-06 15:27:07 +0530

    Adds a start fragment to handle the intial transition

---
+++ app/src/main/java/com/example/loft/fragments/StartFragment.kt
@@ -0,0 +1,22 @@
+package com.example.loft.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navGraphViewModels
+import com.example.loft.R
+
+class StartFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        findNavController().navigate(R.id.action_start_home_fragment)
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
+}
